43c6c3834fe13dcae89abaa94620702397f30fb1
Kemran1 <94720339+Kemran1@users.noreply.github.com>
2024-11-05 17:32:38 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,58 +1,14 @@
-// src/App.js
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import Admin from './Admin';
 import './App.css';
-import { checkAccess } from './googleSheetsAPI';
 
-const App = () => {
-    const [telegramId, setTelegramId] = useState(null);
-    const [accessGranted, setAccessGranted] = useState(null);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const fetchTelegramId = async () => {
-            try {
-                const tg = window.Telegram.WebApp;
-                tg.ready();
-
-                // Получение Telegram ID пользователя
-                const userId = tg.initDataUnsafe.user.id;
-                setTelegramId(userId);
-
-                // Проверка доступа пользователя
-                const access = await checkAccess(userId);
-                setAccessGranted(access);
-
-                setLoading(false);
-            } catch (error) {
-                console.error('Error fetching Telegram ID:', error);
-                setLoading(false);
-            }
-        };
-
-        fetchTelegramId();
-    }, []);
-
+function App() {
     return (
         <div className="App">
-            <h1>Проверка доступа</h1>
-            {loading ? (
-                <p>Загрузка...</p>
-            ) : (
-                <div>
-                    <p>Ваш Telegram ID: {telegramId}</p>
-                    {accessGranted !== null && (
-                        <div>
-                            {accessGranted ? (
-                                <p>Доступ разрешен!</p>
-                            ) : (
-                                <p>Доступ запрещен.</p>
-                            )}
-                        </div>
-                    )}
-                </div>
-            )}
+            <Admin />
         </div>
     );
-};
+}
+
 
 export default App;
